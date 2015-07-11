125b426826bc0ddfa0aaf80b4263c19abb140d79
moshee <moshee@displaynone.us>
2015-07-11 04:52:59 +0000

    shorthash: add memorable hash shortener

--- shorthash/shorthash.go
+++ shorthash/shorthash.go
@@ -1,6 +1,10 @@
 package shorthash
 
-const Chars = "abcdefghijklmnopqrstuvwxyzZYXWVUTSRQPONMLKJIHGFEDCBA1234567890"
+const (
+	Chars      = "abcdefghijklmnopqrstuvwxyzZYXWVUTSRQPONMLKJIHGFEDCBA1234567890"
+	Vowels     = "aeiou"
+	Consonants = "bdfghklmnpqrstvwxyz"
+)
 
 // Make squashes a long hash into a shorter one represented as hashLen base62
 // characters.
@@ -12,6 +16,26 @@ func Make(hash []byte, hashLen int) string {
 	}
 	for i := range s {
 		s[i] = Chars[int(s[i])%len(Chars)]
+	}
+
+	return string(s)
+}
+
+// Memorable produces a shortened output with alternating consonants and
+// vowels.
+func Memorable(hash []byte, hashLen int) string {
+	s := make([]byte, uint(hashLen))
+
+	for i, b := range hash {
+		s[i%hashLen] ^= b
+	}
+
+	for i := range s {
+		if i%2 == 0 {
+			s[i] = Consonants[int(s[i])%len(Consonants)]
+		} else {
+			s[i] = Vowels[int(s[i])%len(Vowels)]
+		}
 	}
 
 	return string(s)
